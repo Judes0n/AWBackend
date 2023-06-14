6388d448417252ba91f16dda4e6aa6520469bf64
Judes0n <judesonjoy111@gmail.com>
2023-06-14 21:06:28 +0530

    Add project files.

---
+++ AWBackend/Models/AwdbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace AWBackend.Models;
+
+public partial class AwdbContext : DbContext
+{
+    private readonly IConfiguration Configuration;
+    public AwdbContext(IConfiguration _configuration)
+    {
+       Configuration = _configuration;
+    }
+
+    public AwdbContext(DbContextOptions<AwdbContext> options , IConfiguration _configuration)
+        : base(options)
+    {
+        Configuration = _configuration;
+    }
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(this.Configuration.GetConnectionString("ConnStr"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Course)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Dob)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ AWBackend/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AWBackend.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public string Dob { get; set; } = null!;
+
+    public string Course { get; set; } = null!;
+}
